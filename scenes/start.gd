f5c0cf28abfd59b1472bfca3c0a8961140ef842a
Manaswi Goyal <104673221+ma-nu03@users.noreply.github.com>
2026-06-24 03:48:52 +0900

    UI stuff

---
+++ scenes/start.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Clockscene.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
