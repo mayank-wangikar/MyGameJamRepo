bb78a7f41711608a620d3c5de58fab9b871516e9
Manaswi Goyal <104673221+ma-nu03@users.noreply.github.com>
2026-06-26 06:21:16 +0900

    pause

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,16 +1,10 @@
 extends Control
 @onready var main_buttons: VBoxContainer = $MainButtons
-@onready var options: Panel = $Options
+@onready var pause_screen: Panel = $PauseScreen
 
 func _ready() -> void:
 	main_buttons.visible = true
-	options.visible = false
-
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Clockscene.tscn")
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+	pause_screen.visible = false
 
 func _on_options_pressed() -> void:
 	main_buttons.visible = false
