1c6a0c45a0ea95a3c8a1652ef357bfe555c562cb
Manaswi Goyal <104673221+ma-nu03@users.noreply.github.com>
2026-06-26 01:23:09 +0900

    UI and music

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,4 +1,10 @@
 extends Control
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
+
+func _ready() -> void:
+	main_buttons.visible = true
+	options.visible = false
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Clockscene.tscn")
@@ -6,6 +12,9 @@ func _on_start_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
+func _on_options_pressed() -> void:
+	main_buttons.visible = false
+	options.visible = true
 
 func _on_button_pressed() -> void:
 	pass # Replace with function body.
